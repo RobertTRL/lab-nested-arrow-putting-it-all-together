7bdf78de6f3d7133d9b5ede518975bf49b78f2fa
Robert Toroitich <robertojnr2802@gmail.com>
2026-03-23 15:39:14 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const { use } = require("react");
 
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  return (passwordAttempt) => {
+    attemptCount++;
+    if (attemptCount <= 3) {
+      if (passwordAttempt === userInfo.password) {
+        return "Login successful"
+      }
+      else {
+        return `Attempt ${attemptCount} : Login failed`
+      }}
+    else {
+      return "Account locked due to too many failed login attempts"
+  }}}
 
 
 module.exports = {
